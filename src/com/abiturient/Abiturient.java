4af12c34780a7f501399179413043958965b11a6
ProFeSSoR <mironzhereb12345@gmail.com>
2023-11-16 16:30:05 +0200

    fix

--- src/com/abiturient/Abiturient.java
+++ src/com/abiturient/Abiturient.java
@@ -64,7 +64,7 @@ public class Abiturient {
         this.averageGrade = averageGrade;
     }
 
-    @Override
+
     public String toString() {
         return "Abiturient{" +
                 "id=" + id +
